812a6e0b79e30e8ec6c0e4c9a264d4b80400b2c6
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2025-02-19 19:30:25 -0300

    Update programs.lua

--- BIOS/alexOS/desktop/system/programs.lua
+++ BIOS/alexOS/desktop/system/programs.lua
@@ -1,8 +1,35 @@
+local buttons = require('models/button')
+
+local monitor = peripheral.find('monitor') or term.current()
 
 local programs = {}
 
-function programs.run()
+local breakLoop = false
 
+function programs.run()
+    while true do
+        buttons.clear()
+        
+        genProgramsWindow()
+        buttons.click()
+
+        if breakLoop then
+            break
+        end
+    end
+end
+
+function genProgramsWindow()
+    monitor.setBackgroundColor(colors.gray)
+    monitor.clear()
+    buttons.create(1,1, 2, 1, returnToDesktop, 'white', '<')
+    buttons.create(2,3, 6, 5, returnToDesktop, 'blue', '<')
+
+    
+end
+
+function returnToDesktop()
+    breakLoop = true
 end
 
 return programs
